de8818b49358bc536a2571f5be9798b7847bafc1
Romulo Ferrer <romulo.ferrer@projeto.lead>
2019-09-25 11:44:16 -0300

    I don't know what happened.

--- application.py
+++ application.py
@@ -1,39 +1,18 @@
-import os
 
-from flask import Flask
+from flask import Flask, render_template, request
+from application import db
+from application.models import Data
 
+# Elastic Beanstalk initalization
+application = Flask(__name__)
+application.debug=True
+# change this to your own value
+application.secret_key = 'dev'   
 
-def create_app(test_config=None):
-    # create and configure the app
-    application = Flask(__name__, instance_relative_config=True)
-    application.config.from_mapping(
-        SECRET_KEY='dev',
-        DEBUG='True'
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-   
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        application.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        application.config.from_mapping(test_config)
+@application.route('/')
+@application.route('/hello')
+def hello():
+    return "Hello, World!"
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(application.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @application.route('/')
-    @application.route('/hello')
-    def hello():
-        return 'Olá, mundo!'
-
-    return application
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
- 
+if __name__ == '__main__':
+    application.run(debug=True)
--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://romulofff:270198Rf@database-dist-caster.cdfcc07ecwn4.us-east-1.rds.amazonaws.com'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://romulofff:270198Rf@database-dist-caster.cdfcc07ecwn4.us-east-1.rds.amazonaws.com/database-dist-caster'
