73f02b5779b88b29b548c47b766c9be65f5da7a0
Rômulo Férrer Filho <romulofff@alu.ufc.br>
2019-10-31 17:58:08 -0300

    Create Dashboard

--- distributedPortal/src/app/podcast.service.ts
+++ distributedPortal/src/app/podcast.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 import { Podcast } from './podcast';
 import { PODCASTS } from './mock-podcasts';
-import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PodcastService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getPodcasts(): Observable<Podcast[]> {
+    this.messageService.add('PodcastService: fetched Podcasts')
     return of(PODCASTS);
   }
 }
