d4e0e864c0719e6e07fd9139ff7772f5d76b18c2
romulofff <romulo_freire_filho@hotmail.com>
2019-11-01 11:45:25 -0300

    Begin pt 6

--- distributedPortal/src/app/podcast.service.ts
+++ distributedPortal/src/app/podcast.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { MessageService } from './message.service';
 import { Podcast } from './podcast';
@@ -11,15 +13,41 @@ import { PODCASTS } from './mock-podcasts';
 })
 export class PodcastService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
+
+  private log(message: string) {
+    this.messageService.add(`PodcastService: ${message}`);
+  }
+
+  private podcastsUrl = 'http://distcast.zrmmwsdctd.us-east-1.elasticbeanstalk.com/';
+  
   getPodcasts(): Observable<Podcast[]> {
-    this.messageService.add('PodcastService: fetched Podcasts')
-    return of(PODCASTS);
+    this.log("fetched podcasts.")
+    return this.http.get<Podcast[]>(this.podcastsUrl)
+      .pipe(
+        catchError(this.handleError<Podcast[]>('getPodcasts', []))
+      );
   }
 
   getPodcast(EpisodeID: number): Observable<Podcast> {
-    this.messageService.add(`PodcastService: fetched podcast EpisodeID=${EpisodeID}`);
+    this.log(`fetched podcast EpisodeID=${EpisodeID}`);
     return of(PODCASTS.find(podcast => podcast.EpisodeID === EpisodeID))
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
